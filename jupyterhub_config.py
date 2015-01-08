8313424b44599163e8d695502ee60c78da1a1bfd
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-01-07 16:22:36 -0800

    Fix config file to use JupyterHub instead of JupyterHubApp

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -5,23 +5,23 @@ import os
 import sys
 
 # Base configuration
-c.JupyterHubApp.log_level = 10
-c.JupyterHubApp.admin_users = admin = set()
+c.JupyterHub.log_level = 10
+c.JupyterHub.admin_users = admin = set()
 
 # Configure the authenticator
-c.JupyterHubApp.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 c.Authenticator.whitelist = whitelist = set()
 
 # Configure the spawner
-c.JupyterHubApp.spawner_class = 'dockerspawner.SystemUserSpawner'
+c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 c.SystemUserSpawner.user_ids = userids = dict()
 c.SystemUserSpawner.container_image = 'jhamrick/systemuser'
 
 # The docker instances need access to the Hub, so the default loopback port
 # doesn't work:
 from IPython.utils.localinterfaces import public_ips
-c.JupyterHubApp.hub_ip = public_ips()[0]
+c.JupyterHub.hub_ip = public_ips()[0]
 
 # Add users to the admin list, the whitelist, and also record their user ids
 root = os.environ.get('OAUTHENTICATOR_DIR', os.path.dirname(__file__))
